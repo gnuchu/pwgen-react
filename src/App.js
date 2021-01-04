a8bd7c4de8c6efb9fb8d18c4125ba626cb09c08d
John Griffiths <gnuchu@gmail.com>
2021-01-04 13:32:48 +0000

    Working app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import './App.css';
 import { generatePassword } from './Functions'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import $ from 'jquery';
 
 class App extends React.Component {
 
   state = {
     password: generatePassword(20),
-    length: 20
+    length: 20,
+    useUpper: true,
+    useLower: true,
+    useNumbers: true,
+    useSpecial: true
+  }
+
+  onChangeUseUpper = (event) => {
+    this.setState(initialState => ({
+      useUpper: !initialState.useUpper,
+    }))
+  }
+
+  onChangeUseLower = () => {
+    this.setState(initialState => ({
+      useLower: !initialState.useLower
+    }))
+  }
+
+  onChangeUseNumbers = () => {
+    this.setState(initialState => ({
+      useNumbers: !initialState.useNumbers
+    }))
+  }
+
+  onChangeUseSpecial = () => {
+    this.setState(initialState => ({
+      useSpecial: !initialState.useSpecial
+    }))
   }
 
   generateNewPassword = () => {
@@ -35,6 +64,12 @@ class App extends React.Component {
     const password = passwordBox.value;
 
     navigator.clipboard.writeText(password);
+    
+    let notificationDiv = document.getElementById("copied-notification")
+    notificationDiv.innerHTML = "Copied!"
+
+    $('#copied-notification').show()
+    $('#copied-notification').fadeOut(5000)
   }
 
   selectChangedHandler(event) {
@@ -47,39 +82,72 @@ class App extends React.Component {
         <div className="container-fluid">
           <div className="jumbotron">
             <div className="row">
-              <div className="col-1">
+              <div className="col-3">
               </div>
               <div className="col"> 
                 <div className="row d-flex justify-content-center title">
-                  <h1>Password Generator</h1>
+                  <h3>Password Generator</h3>
                 </div>
                 <div className="row d-flex justify-content-center">
-                <form>
-                  <div className="form-group mb-3 flex-nowrap">
-                    <div className="input-group-prepend">
-                      <div className="form-check form-check-inline">
-                        <input onChange={(event)=>this.selectChangedHandler(event)} className="form-check-input" type="checkbox" id="lowerCheck" checked></input>
-                        <label className="form-check-label" htmlFor="lowerCheck">Uppercase</label>
-                      </div>
-                      <div className="form-check form-check-inline">
-                        <input onChange={(event)=>this.selectChangedHandler(event)} className="form-check-input" type="checkbox" id="upperCheck" checked></input>
-                        <label className="form-check-label" htmlFor="upperCheck">Lowercase</label>
-                      </div>
-                      <div className="form-check form-check-inline">
-                        <input onChange={(event)=>this.selectChangedHandler(event)} className="form-check-input" type="checkbox" id="numberCheck" checked></input>
-                        <label className="form-check-label" htmlFor="numberCheck">Numbers</label>
-                      </div>
-                      <div className="form-check form-check-inline">
-                        <input onChange={(event)=>this.selectChangedHandler(event)} className="form-check-input" type="checkbox" id="specialCheck" checked></input>
-                        <label className="form-check-label" htmlFor="specialCheck">Special</label>
+                  <div className="checkBoxes">
+                    {/* <div className="form-group mb-3 flex-nowrap"> */}
+                      <div className="input-group-prepend">
+                        <div className="form-check form-check-inline">
+                          <input 
+                            checked={this.state.useUpper}
+                            onChange={this.onChangeUseUpper}
+                            className="form-check-input"
+                            type="checkbox"
+                            id="upperCheck">
+                          </input>
+                          <label className="form-check-label" htmlFor="upperCheck">Uppercase</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input 
+                            checked={this.state.useLower}
+                            onChange={this.onChangeUseLower}
+                            className="form-check-input" 
+                            type="checkbox" 
+                            id="lowerCheck">
+                          </input>
+                          <label className="form-check-label" htmlFor="lowerCheck">Lowercase</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input 
+                            checked={this.state.useNumbers}
+                            onChange={this.onChangeUseNumbers}
+                            className="form-check-input"
+                            type="checkbox"
+                            id="numberCheck">
+                          </input>
+                          <label className="form-check-label" htmlFor="numberCheck">Numbers</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input 
+                            checked={this.state.useSpecial}
+                            onChange={this.onChangeUseSpecial}
+                            className="form-check-input"
+                            type="checkbox"
+                            id="specialCheck">
+                          </input>
+                          <label className="form-check-label" htmlFor="specialCheck">Special</label>
+                        </div>
+                        
+                        <input
+                          type="number"
+                          className="form-control password-input input-box-center"
+                          aria-label="Password Length"
+                          value={this.state.length}
+                          onChange={(event) => this.handleLength(event)}>
+                        </input>
                       </div>
                     </div>
-                    
+                      
                     <div className="input-group mb-3">
                       <input 
                         id="password-box"
                         type="text" 
-                        className="form-control" 
+                        className="form-control input-box-center"
                         placeholder="Password" 
                         aria-label="Password"
                         value={this.state.password}
@@ -89,38 +157,32 @@ class App extends React.Component {
 
                       <div className="input-group-append">
                         <button 
+                          type="button" 
+                          className="btn btn-light float-right"
+                          onClick={this.generateNewPassword}>
+                        
+                          Generate
+
+                        </button>
+                      </div>
+
+                      <div className="input-group-append">
+                        <button 
                           className="btn btn-outline-secondary" 
                           type="button" 
                           id="copy-button"
+                          aria-label="Copy"
                           onClick={this.copyPassword}>
                           
                           <FontAwesomeIcon icon={faCopy} />
 
                         </button>
                       </div>
+                      <div id="copied-notification"></div>
                     </div>
-
-                    <input
-                      type="number"
-                      className="form-control password-input"
-                      aria-label="Password Length"
-                      value={this.state.length}
-                      onChange={(event) => this.handleLength(event)}>
-                    </input>
-
-                    <button 
-                      type="button" 
-                      className="btn btn-primary float-right"
-                      onClick={this.generateNewPassword}>
-                      
-                      Generate
-
-                    </button>
-                  </div>
-                </form>
                 </div>
               </div>
-              <div className="col-1">
+              <div className="col-3">
               </div>
             </div>
           </div>
