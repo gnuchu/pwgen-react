c31526687ed484d9252272a4058bf85fbfd24f24
John Griffiths <gnuchu@gmail.com>
2021-01-07 22:50:24 +0000

    Updates to layout for mobile. Checkboxes not working

--- src/App.js
+++ src/App.js
@@ -6,44 +6,72 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import $ from 'jquery';
 
+
+// const subtractTwoArrays = (arr1, arr2) => {
+//   arr1.filter( el => !arr2.includes(el) )
+// }
+
+const initialState = {
+  upper: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+  lower: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  numbers: ['0','1','2','3','4','5','6','7','8','9'],
+  special: ['!','@','£','$','^','-','_','+','=','|',':',';','<','>','~'],
+  allChars: [],
+  length: 20,
+  useUpper: true,
+  useLower: true,
+  useNumbers: true,
+  useSpecial: true,
+  password: "password"
+}
+
 class App extends React.Component {
 
-  state = {
-    password: generatePassword(20),
-    length: 20,
-    useUpper: true,
-    useLower: true,
-    useNumbers: true,
-    useSpecial: true
+  constructor(props) {
+    super(props);
+    this.state = initialState
   }
-
+  
+  componentDidMount() {
+    const allChars = this.state.upper.concat(this.state.lower, this.state.numbers, this.state.special)
+    this.setState({
+      allChars: allChars
+    })
+  }
+  
   onChangeUseUpper = (event) => {
     this.setState(initialState => ({
       useUpper: !initialState.useUpper,
+      password: generatePassword(this.state)
     }))
+    
   }
 
   onChangeUseLower = () => {
     this.setState(initialState => ({
-      useLower: !initialState.useLower
+      useLower: !initialState.useLower,
+      password: generatePassword(this.state)
     }))
   }
 
   onChangeUseNumbers = () => {
     this.setState(initialState => ({
-      useNumbers: !initialState.useNumbers
+      useNumbers: !initialState.useNumbers,
+      password: generatePassword(this.state)
     }))
   }
 
   onChangeUseSpecial = () => {
     this.setState(initialState => ({
-      useSpecial: !initialState.useSpecial
+      useSpecial: !initialState.useSpecial,
+      password: generatePassword(this.state)
     }))
   }
 
   generateNewPassword = () => {
+    console.log(this.state)
     this.setState({
-      password: generatePassword(this.state.length)
+      password: generatePassword(this.state)
     })
   }
 
@@ -55,7 +83,8 @@ class App extends React.Component {
     let newLength = Number(event.target.value);
     newLength = newLength > 77 ? 77 : newLength;
     this.setState({
-      length: newLength
+      length: newLength,
+      password: generatePassword(this.state)
     })
   }
 
@@ -72,82 +101,87 @@ class App extends React.Component {
     $('#copied-notification').fadeOut(5000)
   }
 
-  selectChangedHandler(event) {
-    console.log(event);
-  }
-
   render() {
     return (
       <div className="App">
         <div className="container-fluid">
           <div className="jumbotron">
             <div className="row">
-              <div className="col-3">
-              </div>
-              <div className="col"> 
-                <div className="row d-flex justify-content-center title">
+              <div className="col col-sm-12">
+                <center>
                   <h3>Password Generator</h3>
-                </div>
-                <div className="row d-flex justify-content-center">
-                  <div className="checkBoxes">
-                    {/* <div className="form-group mb-3 flex-nowrap"> */}
-                      <div className="input-group-prepend">
-                        <div className="form-check form-check-inline">
-                          <input 
-                            checked={this.state.useUpper}
-                            onChange={this.onChangeUseUpper}
-                            className="form-check-input"
-                            type="checkbox"
-                            id="upperCheck">
-                          </input>
-                          <label className="form-check-label" htmlFor="upperCheck">Uppercase</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                          <input 
-                            checked={this.state.useLower}
-                            onChange={this.onChangeUseLower}
-                            className="form-check-input" 
-                            type="checkbox" 
-                            id="lowerCheck">
-                          </input>
-                          <label className="form-check-label" htmlFor="lowerCheck">Lowercase</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                          <input 
-                            checked={this.state.useNumbers}
-                            onChange={this.onChangeUseNumbers}
-                            className="form-check-input"
-                            type="checkbox"
-                            id="numberCheck">
-                          </input>
-                          <label className="form-check-label" htmlFor="numberCheck">Numbers</label>
-                        </div>
-                        <div className="form-check form-check-inline">
-                          <input 
-                            checked={this.state.useSpecial}
-                            onChange={this.onChangeUseSpecial}
-                            className="form-check-input"
-                            type="checkbox"
-                            id="specialCheck">
-                          </input>
-                          <label className="form-check-label" htmlFor="specialCheck">Special</label>
-                        </div>
-                        
-                        <input
-                          type="number"
-                          className="form-control password-input input-box-center"
-                          aria-label="Password Length"
-                          value={this.state.length}
-                          onChange={(event) => this.handleLength(event)}>
+                </center>
+                <div className="checkBoxes">
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="form-check form-check-inline">
+                        <input 
+                          checked={this.state.useUpper}
+                          onChange={this.onChangeUseUpper}
+                          className="form-check-input"
+                          type="checkbox"
+                          id="upperCheck">
                         </input>
+                        <label className="form-check-label" htmlFor="upperCheck">Uppercase</label>
                       </div>
                     </div>
-                      
-                    <div className="input-group mb-3">
+                    
+                    <div className="col-6">
+                      <div className="form-check form-check-inline">
+                        <input 
+                          checked={this.state.useLower}
+                          onChange={this.onChangeUseLower}
+                          className="form-check-input" 
+                          type="checkbox" 
+                          id="lowerCheck">
+                        </input>
+                        <label className="form-check-label" htmlFor="lowerCheck">Lowercase</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-6">
+                      <div className="form-check form-check-inline">
+                        <input 
+                          checked={this.state.useNumbers}
+                          onChange={this.onChangeUseNumbers}
+                          className="form-check-input"
+                          type="checkbox"
+                          id="numberCheck">
+                        </input>
+                        <label className="form-check-label" htmlFor="numberCheck">Numbers</label>
+                      </div>
+                    </div>
+                    <div className="col-6">
+                      <div className="form-check form-check-inline">
+                        <input 
+                          checked={this.state.useSpecial}
+                          onChange={this.onChangeUseSpecial}
+                          className="form-check-input"
+                          type="checkbox"
+                          id="specialCheck">
+                        </input>
+                        <label className="form-check-label" htmlFor="specialCheck">Special</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col">
+                    <input
+                      type="number"
+                      className="form-control input-margins input-box-center"
+                      aria-label="Password Length"
+                      value={this.state.length}
+                      onChange={(event) => this.handleLength(event)}>
+                    </input>
+
+                    <div className="input-group">
                       <input 
                         id="password-box"
                         type="text" 
-                        className="form-control input-box-center"
+                        className="form-control input-box-center input-margins"
                         placeholder="Password" 
                         aria-label="Password"
                         value={this.state.password}
@@ -155,34 +189,39 @@ class App extends React.Component {
                         readOnly>
                       </input>
 
-                      <div className="input-group-append">
-                        <button 
-                          type="button" 
-                          className="btn btn-light float-right"
-                          onClick={this.generateNewPassword}>
+                      <button 
+                        className="btn btn-outline-secondary input-margins"
+                        type="button" 
+                        id="copy-button"
+                        aria-label="Copy"
+                        onClick={this.copyPassword}>
                         
-                          Generate
+                        <FontAwesomeIcon icon={faCopy} />
 
-                        </button>
-                      </div>
+                      </button>
 
-                      <div className="input-group-append">
-                        <button 
-                          className="btn btn-outline-secondary" 
-                          type="button" 
-                          id="copy-button"
-                          aria-label="Copy"
-                          onClick={this.copyPassword}>
-                          
-                          <FontAwesomeIcon icon={faCopy} />
-
-                        </button>
-                      </div>
-                      <div id="copied-notification"></div>
                     </div>
+                  </div>
                 </div>
-              </div>
-              <div className="col-3">
+                  
+                <div className="row">
+                  <div className="col col-sm-4"></div>
+
+                  <div className="col col-sm-4">
+                    <button 
+                      type="button" 
+                      className="btn btn-primary float-right"
+                      onClick={this.generateNewPassword}>
+                    
+                      Generate
+
+                    </button>
+                  </div>
+
+                  <div className="col col-sm-4">
+                    <div id="copied-notification"></div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/Functions.js
+++ src/Functions.js
@@ -1,28 +1,29 @@
-const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-const lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-const numbers = ['0','1','2','3','4','5','6','7','8','9']
-const special = ['!','@','£','$','^','-','_','+','=','|',':',';','<','>','~']
-const allChars = upper.concat(lower, numbers, special)
 
-export function generatePassword(length) {
+export function generatePassword(settings) {
   let validPass = false
   let pass = ""
 
+  // createCharSet(settings)
+
+  if(settings.allChars.length === 0) {
+    return "At least 1 character set must be selected"
+  }
+
   while(!validPass) {
-    pass = generate(length)
-    validPass = validatePassword(pass)
+    pass = generate(settings)
+    validPass = validatePassword(pass, settings)
   }
 
   return pass
 }
 
-var generate = function(length) {
+var generate = function(settings) {
   let pass = ""
   let i = 0
-  const shuffledChars = shuffleArray(allChars)
+  const shuffledChars = shuffleArray(settings.allChars)
 
-  for(i=0; i<length; i++) {
-    var pos = Math.ceil(Math.random() * (length-1))
+  for(i=0; i<settings.length; i++) {
+    var pos = Math.ceil(Math.random() * (settings.length-1))
     pass += shuffledChars[pos]
   }
   return pass
@@ -37,11 +38,11 @@ var shuffleArray = function (a) {
   })
 }
 
-var validatePassword = function(pass) {
-  const hasUpper    = hasOne(pass, upper)
-  const hasLower    = hasOne(pass, lower)
-  const hasSpecial  = hasOne(pass, special)
-  const hasNumber   = hasOne(pass, numbers)
+var validatePassword = function(pass, settings) {
+  const hasUpper    = hasOne(pass, settings.upper)
+  const hasLower    = hasOne(pass, settings.lower)
+  const hasSpecial  = hasOne(pass, settings.special)
+  const hasNumber   = hasOne(pass, settings.numbers)
   
   return (hasUpper && hasLower && hasSpecial && hasNumber)
 
