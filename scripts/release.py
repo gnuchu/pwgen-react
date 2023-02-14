917060abebf9f4b7335884c238e1ed58702faa57
John Griffiths <gnuchu@gmail.com>
2023-02-14 01:07:48 +0000

    Create release zip file

--- scripts/release.py
+++ scripts/release.py
@@ -5,13 +5,23 @@ import pathlib
 import sys
 from zipfile import ZipFile
 
-def main():
-    where_I_am = pathlib.Path.cwd()
-    project_root = os.path.join(where_I_am, "..")
-    build_directory = os.path.join(project_root, "build")
-    print(build_directory)
+def main(release_number):
+    project_root = pathlib.Path.cwd()
+    build_directory = "build"
+    release_directory = os.path.join(project_root, "releases")
+    release_name = f"pwgen_react_{release_number}.zip"
+    release = os.path.join(release_directory, release_name)
 
+    try:
+        with ZipFile(release, "w") as zip_object:
+            for (dirPath, dirName, fileNames) in os.walk(build_directory):
+                for fileName in fileNames:
+                    file = os.path.join(dirPath, fileName)
+                    zip_object.write(file)
+        
+    except Exception as e:
+        raise(e)
 
 if __name__ == "__main__":
-    print(sys.argv)
-    main()
+    release_number = sys.argv[1]
+    main(release_number)
